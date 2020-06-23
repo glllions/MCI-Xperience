e516ae420033001252f740315423dfd365ce9007
Gabriel Löwen <gabriel.loewen@smail.th-koeln.de>
2020-06-23 12:53:51 +0200

    Fxmls erweitert

--- src/login/LoginFXMLController.java
+++ src/login/LoginFXMLController.java
@@ -1,4 +1,21 @@
 package login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
 public class LoginFXMLController {
+
+    @FXML
+    private StackPane root;
+
+    @FXML
+    private Button buttonLogin;
+
+
+    @FXML
+    private void handleButtonLogin(ActionEvent event) {
+
+    }
 }
